d271294d9aae865b45fcec5918e07101450c818b
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-06-30 15:27:01 -0300

    Feat: add custom query method to RouterRepository for enhanced router retrieval

--- scannerdevices-service/src/main/kotlin/com/net/scannerdevices_service/DataBase/Repository/RouterRepository.kt
+++ scannerdevices-service/src/main/kotlin/com/net/scannerdevices_service/DataBase/Repository/RouterRepository.kt
@@ -2,6 +2,9 @@ package com.net.scannerdevices_service.DataBase.Repository
 
 import com.net.scannerdevices_service.DataBase.Entity.Router
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Optional
 
 interface RouterRepository : JpaRepository<Router, Long> {
+    fun findBySsidAndProtocolAndSecurityType(ssid: String, protocol: String, securityType: String): Router?
+    override fun findById(id: Long): Optional<Router>
 }

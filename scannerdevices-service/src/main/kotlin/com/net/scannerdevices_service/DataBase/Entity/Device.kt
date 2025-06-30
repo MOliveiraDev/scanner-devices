12fc7df648406eb4c1fed74e0b418fc8e2b1c819
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-06-30 13:00:49 -0300

    Add Device entity with JPA annotations

---
+++ scannerdevices-service/src/main/kotlin/com/net/scannerdevices_service/DataBase/Entity/Device.kt
@@ -0,0 +1,30 @@
+package com.net.scannerdevices_service.DataBase.Entity
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import jakarta.persistence.FetchType
+import jakarta.persistence.JoinColumn
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "devices_tb")
+class Device (
+
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    val id: Long = 0,
+
+    val ip: String,
+    val device: String,
+    val connected: Boolean,
+    val lastConnection: LocalDateTime? = null,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "router_id")
+    val router: Router
+
+
+)

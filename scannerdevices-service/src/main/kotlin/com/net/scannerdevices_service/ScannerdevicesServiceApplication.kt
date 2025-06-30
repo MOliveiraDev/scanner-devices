6c1b316e31b414fa614614c0b22366f3fb09368d
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-06-30 11:13:54 -0300

    first commit

---
+++ scannerdevices-service/src/main/kotlin/com/net/scannerdevices_service/ScannerdevicesServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.net.scannerdevices_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ScannerdevicesServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ScannerdevicesServiceApplication>(*args)
+}
---
+++ scannerdevices-service/src/test/kotlin/com/net/scannerdevices_service/ScannerdevicesServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.net.scannerdevices_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ScannerdevicesServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

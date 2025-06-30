23306291d61f3b747e0c003d2ce45283300290b9
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-06-30 17:15:47 -0300

    Feat: add endpoints to retrieve devices by router ID and all devices for all routers

--- scannerdevices-service/src/main/kotlin/com/net/scannerdevices_service/Controller/NetworkScanController.kt
+++ scannerdevices-service/src/main/kotlin/com/net/scannerdevices_service/Controller/NetworkScanController.kt
@@ -1,5 +1,6 @@
 package com.net.scannerdevices_service.Controller
 
+import com.net.scannerdevices_service.DTO.DeviceResult
 import com.net.scannerdevices_service.DTO.RouterResult
 import com.net.scannerdevices_service.DTO.ScanResult
 import com.net.scannerdevices_service.DataBase.Repository.RouterRepository
@@ -24,14 +25,51 @@ class NetworkScanController(
         return ResponseEntity.ok().build()
     }
 
-    @GetMapping("/scan-all")
-    fun getLastScan(): ResponseEntity<ScanResult> {
-        return ResponseEntity.ok(networkScanService.getLastScanResult())
-    }
-
     @GetMapping("/routers/{id}")
     fun getRouterById(@PathVariable id: Long): ResponseEntity<RouterResult> {
         val router = routerRepository.findById(id).orElseThrow { RuntimeException("Roteador não foi encontrado") }
         return ResponseEntity.ok(RouterResult(router.id, router.ssid, router.protocol, router.securityType))
+    }
+
+    @GetMapping("/Devices/{routerId}")
+    fun getDevicesByRouterId(@PathVariable routerId: Long): ResponseEntity<List<DeviceResult>> {
+        val devices = routerRepository.findById(routerId)
+            .orElseThrow { RuntimeException("Roteador não foi encontrado") }
+            .devices.map { device ->
+                DeviceResult(
+                    ip = device.ip,
+                    routerId = device.router.id,
+                    device = device.device,
+                    connected = device.connected,
+                    lastConnection = device.lastConnection
+                )
+            }
+        return ResponseEntity.ok(devices)
+    }
+
+    @GetMapping("/AllRouters")
+    fun getAllRouters(): ResponseEntity<List<RouterResult>> {
+        val routers = routerRepository.findAll().map { RouterResult(it.id, it.ssid, it.protocol, it.securityType) }
+        return ResponseEntity.ok(routers)
+    }
+
+    @GetMapping("/all-devices-for-routers")
+    fun getAllDevices(): ResponseEntity<List<ScanResult>> {
+        val routers = routerRepository.findAll()
+        val scanResults = routers.map { router ->
+            ScanResult(
+                RouterResult(router.id, router.ssid, router.protocol, router.securityType),
+                router.devices.map { device ->
+                    DeviceResult(
+                        ip = device.ip,
+                        routerId = device.router.id,
+                        device = device.device,
+                        connected = device.connected,
+                        lastConnection = device.lastConnection
+                    )
+                }
+            )
+        }
+        return ResponseEntity.ok(scanResults)
     }
 }
